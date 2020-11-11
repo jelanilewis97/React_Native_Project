68cacf188fdbc60a2f6bc877b32c30064734848a
jelanilewis97 <43737512+jelanilewis97@users.noreply.github.com>
2020-11-10 22:18:49 -0800

    Update App.js

--- App.js
+++ App.js
@@ -12,35 +12,6 @@ const client_id = 'ba15c66b1d794aaea65e0dd13d57b465'; // Your client id
 const client_secret = '4370c3eb6f594096bc278cde0424ac7c'; // Your secret
 const redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
-function MyTabs() {
-  return (
-    <Tabs.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
-
-          if (route.name === 'Home') {
-            iconName = focused
-              ? 'ios-information-circle'
-              : 'ios-information-circle-outline';
-          } else if (route.name === 'Settings') {
-            iconName = focused ? 'ios-list-box' : 'ios-list';
-          }
-
-          // You can return any component that you like here!
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-      }}
-    >
-      <Tabs.Screen name="Home" component={Feed} />
-      <Tabs.Screen name="Settings" component={Feed} />
-    </Tabs.Navigator>
-  )
-}
 
 export default function App() {
   return (
@@ -57,7 +28,6 @@ export default function App() {
             iconName = focused ? 'ios-list-box' : 'ios-list';
           }
 
-          // You can return any component that you like here!
           return <Ionicons name={iconName} size={size} color={color} />;
         },
       })}
